d6ee8009de25527e92a1998069e26587f6e93d42
Moklesur Rahman <developer.moklesur@gmail.com>
2022-01-18 10:07:46 +0600

    update

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ document.querySelector('#ewallet-form').addEventListener('submit', function(e){
     // stop from reload 
     e.preventDefault()
 
+
+
+
     // get data from inbut box 
     const type = document.querySelector('.add__type').value
     const desc = document.querySelector('.add__description').value
@@ -39,13 +42,16 @@ document.querySelector('#ewallet-form').addEventListener('submit', function(e){
 addItemsToLS(desc, time, type, value)
 
 
-
+  
 
 
 
 
 
 })
+  // show items
+  showItems()
+
 
 // reset form function 
 function resetForm(){
@@ -70,9 +76,10 @@ function getFormatedTime(){
     return formatedTime
 }
 
-// addItemsToLS function
 
-function addItemsToLS(desc, time, type, value){
+
+// get items from LS
+function getItemsFromLS(){
     let items = localStorage.getItem('items')
 
     if(items){
@@ -80,7 +87,12 @@ function addItemsToLS(desc, time, type, value){
     }else{
         items = []
     }
+    return items
+}
+// addItemsToLS function
 
+function addItemsToLS(desc, time, type, value){
+    let items = getItemsFromLS()
     items.push({
         desc: desc,
         time: time,
@@ -89,4 +101,27 @@ function addItemsToLS(desc, time, type, value){
     })
 
     localStorage.setItem('items', JSON.stringify(items))
+}
+
+// show items function
+function showItems(){
+    let items = getItemsFromLS()
+    const collection = document.querySelector('.collection')
+    for(let item of items){
+        const newItem = `<div class="item">
+                        <div class="item-description-time">
+                        <div class="item-description">
+                            <p>${item.desc}</p>
+                        </div>
+                        <div class="item-time">
+                            <p>${item.time}</p>
+                        </div>
+                        </div>
+                        <div class="item-amount ${item.type === '+' ? 'income-amount' : 'expense-amount'}">
+                        <p>${item.type}$${item.value}</p>
+                        </div>
+                    </div>`
+
+                    collection.insertAdjacentHTML('afterbegin', newItem)
+    }
 }
